ea50a2c449d1a4e3dde362c200c5182cfb99adfc
Production <root@my.seos24.ru>
2016-05-19 08:55:51 +0300

    Fixed RequestErrorException arguments in query method

--- src/GoogleClient.php
+++ src/GoogleClient.php
@@ -107,7 +107,7 @@ class GoogleClient
                 if ($errorDom->isCaptcha()) {
                     throw new GoogleCaptchaException(new GoogleCaptcha($errorDom));
                 } else {
-                    throw new RequestErrorException($errorDom);
+                    throw new RequestErrorException();
                 }
             }
         }
