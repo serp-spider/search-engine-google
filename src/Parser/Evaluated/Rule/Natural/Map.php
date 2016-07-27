6ee1ad0906af4bef7d8236a294d7b20d04c92cb0
lmahesh5 <maheshkumar@maximizer-e-services.com>
2016-07-27 15:12:20 +0530

    local results xpath changes

--- src/Parser/Evaluated/Rule/Natural/Map.php
+++ src/Parser/Evaluated/Rule/Natural/Map.php
@@ -18,7 +18,7 @@ class Map implements ParsingRuleInterace
 
     public function match(GoogleDom $dom, \DOMElement $node)
     {
-        if ($dom->getXpath()->query("descendant::div[@class='_Xhb']", $node)->length == 1) {
+        if ($dom->getXpath()->query("descendant::div[@class='_M4k']", $node)->length == 1) {
             return self::RULE_MATCH_MATCHED;
         }
         return self::RULE_MATCH_NOMATCH;
@@ -28,10 +28,24 @@ class Map implements ParsingRuleInterace
     {
 
         $xPath = $dom->getXpath();
+        /*
+         * Samples taken to parse :
+         * https://www.google.co.in/search?q=schools+near+by&start=0&uule=w+CAIQICIHQ2hlbm5haQ&gws_rd=cr
+         * https://www.google.co.in/search?q=beauty+parlours+near+by&start=0&uule=w+CAIQICIHQ2hlbm5haQ&gws_rd=cr
+         * https://www.google.co.in/search?q=restaurants+near+by&start=0&uule=w+CAIQICIHQ2hlbm5haQ&gws_rd=cr
+         */
+        /*
+         * changes in vendor/serps/search-engine-google/src/Parser/Evaluated/Rule/Natural/Map.php
+         * These changes are for Local Results (with MAP)
+         * match function : class='_Xhb' to class='_M4k'
+         * localPack array : div[@class="_oL"]/div to div[@class="_gt"]
+         * url array : 'descendant::div[@class="_gt"]/a' to 'descendant::a'
+         * street array : added new xpath as alternate option
+         */
 
         $item = [
             'localPack' => function () use ($xPath, $node, $dom) {
-                $localPackNodes = $xPath->query('descendant::div[@class="_oL"]/div', $node);
+                $localPackNodes = $xPath->query('descendant::div[@class="_gt"]', $node);
                 $data = [];
                 foreach ($localPackNodes as $localPack) {
                     $data[] = new BaseResult(NaturalResultType::MAP_PLACE, $this->parseItem($localPack, $dom));
@@ -53,6 +67,7 @@ class Map implements ParsingRuleInterace
 
     private function parseItem($localPack, GoogleDom $dom)
     {
+
         return [
             'title' => function () use ($localPack, $dom) {
                 $item = $dom->cssQuery('._rl', $localPack)->item(0);
@@ -62,7 +77,7 @@ class Map implements ParsingRuleInterace
                 return null;
             },
             'url' => function () use ($localPack, $dom) {
-                $item = $dom->getXpath()->query('descendant::div[@class="_gt"]/a', $localPack)->item(1);
+                $item = $dom->getXpath()->query('descendant::a', $localPack)->item(1);
                 if ($item) {
                     return $item->getAttribute('href');
                 }
@@ -71,6 +86,13 @@ class Map implements ParsingRuleInterace
             'street' => function () use ($localPack, $dom) {
                 $item = $dom->getXpath()->query(
                     'descendant::div[@class="_iPk"]/span[@class="rllt__details"]/div[3]/span',
+                    $localPack
+                )->item(0);
+                if ($item) {
+                    return $item->nodeValue;
+                }
+                $item = $dom->getXpath()->query(
+                    'descendant::div[@class="_iPk _Ml"]/span[@class="rllt__details"]/div[3]/span',
                     $localPack
                 )->item(0);
                 if ($item) {
