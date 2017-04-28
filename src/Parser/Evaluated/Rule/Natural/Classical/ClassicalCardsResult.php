444ed588ae40b0b6e08e8309d6687461852659a2
Soufiane  Ghzal <sghzal@gmail.com>
2017-04-28 23:21:29 +0200

    fix google update for mobile classical results  fix #63

--- src/Parser/Evaluated/Rule/Natural/Classical/ClassicalCardsResult.php
+++ src/Parser/Evaluated/Rule/Natural/Classical/ClassicalCardsResult.php
@@ -17,10 +17,8 @@ class ClassicalCardsResult extends ClassicalResult
     public function match(GoogleDom $dom, \DOMElement $node)
     {
         if ($node->getAttribute('class') == 'mnr-c' && null !== $node->childNodes->item(0)) {
-            foreach ($node->childNodes->item(0)->childNodes as $node) {
-                if ($node instanceof \DOMElement && $node->getAttribute('class') == 'rc') {
-                    return self::RULE_MATCH_MATCHED;
-                }
+            if ($dom->cssQuery('.rc', $node)->length == 1) {
+                return self::RULE_MATCH_MATCHED;
             }
         }
         return self::RULE_MATCH_NOMATCH;
