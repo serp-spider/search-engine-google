ac0980cadd0d0a222b3f38ba37806a146872ed31
kuro roli <kuroridoplayer@gmail.com>
2019-06-18 03:30:42 +0800

    Update Map.php
    fix issue Trying to get property 'parentNode' of non-object

--- src/Parser/Evaluated/Rule/Natural/Map.php
+++ src/Parser/Evaluated/Rule/Natural/Map.php
@@ -43,7 +43,6 @@ class Map implements ParsingRuleInterface
                         return $dom->getUrl()->resolveAsString($mapATag->getAttribute('href'));
                     }
                 }
-                return null;
             }
 
         ];
