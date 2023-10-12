819b6dd7aff20888cff0679db45e0cba4917db8f
liviuseomonitor <liviu.stoicescu@seomonitor.ro>
2023-10-12 18:22:10 +0300

    undo bad flights fix

--- src/Parser/Evaluated/Rule/Natural/Flights.php
+++ src/Parser/Evaluated/Rule/Natural/Flights.php
@@ -36,10 +36,11 @@ class Flights implements \Serps\SearchEngine\Google\Parser\ParsingRuleInterface
             return false;
         }
 
+        //NOT TRUE!!! MjjYud is a naturalt result class
         //MjjYud - this is a kowledge graph class. if the flights class is inside a kowledge graph it's a how to get there option, not flight
-        if ($googleDOM->xpathQuery("ancestor::div[contains(concat(' ', normalize-space(@class), ' '), ' MjjYud ')]", $node)->length >0) {
+/*        if ($googleDOM->xpathQuery("ancestor::div[contains(concat(' ', normalize-space(@class), ' '), ' MjjYud ')]", $node)->length >0) {
             return false;
-        }
+        }*/
 
         $urls = $googleDOM->getXpath()->query('descendant::a', $node->firstChild);
         $item = [];
